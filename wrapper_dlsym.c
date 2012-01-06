308cf82215c31fafda70009be563d1d14119f193
Magestik <bastien@magestik.fr>
2012-01-06 21:54:27 +0100

    trying to fix dlsym issue

--- wrapper_dlsym.c
+++ wrapper_dlsym.c
@@ -2,13 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "wrappers.h"
-
+extern void *__libc_dlsym(void *__map, __const char *__name); 
 
 void *dlsym(void *handle, const char *symbol) {
-	//if(DEBUG) 
 	fprintf(stderr, "dlsym(%s)\n", symbol);
-		
-	void *result = __libc_dlsym(handle, symbol); // the replacement we are going to use
-	return result;
+	return __libc_dlsym(handle, symbol);
 }
+
+// gcc -Wall -fPIC -shared -o dlsymWrapper.so dlsym.c -ldl
